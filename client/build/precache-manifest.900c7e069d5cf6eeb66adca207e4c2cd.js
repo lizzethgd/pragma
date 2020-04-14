1f046779c6c84e54cadc104b6fe3e137fbe431e6
lizzethgd <lizzethgd@gmail.com>
2020-04-14 03:09:40 +0300

    make it better2

---
+++ client/build/precache-manifest.900c7e069d5cf6eeb66adca207e4c2cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a97553fe93d9b685ee9d5f33cc763815",
+    "url": "/index.html"
+  },
+  {
+    "revision": "066672c816165d003e67",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ffd15a74e390e1848ef4",
+    "url": "/static/js/2.c1390a1b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c1390a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "066672c816165d003e67",
+    "url": "/static/js/main.d9d2a64e.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
